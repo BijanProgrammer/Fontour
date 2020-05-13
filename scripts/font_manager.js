8e0fa75b14f2284fc38f103246305afeecc9eb8d
BijanProgrammer <bijaneisapour@gmail.com>
2020-05-13 12:39:20 +0430

    Added group changer drop-down menu

--- scripts/font_manager.js
+++ scripts/font_manager.js
@@ -18,6 +18,9 @@ const GROUP_NAMES = {
 
 const GROUP_ELEMENTS = {};
 
+const groupChangerElement = document.getElementById('group-changer');
+let clickedRowElement;
+
 function groupFonts(fontsListElement, rows) {
 	for (const groupName in GROUP_NAMES) {
 		GROUP_ELEMENTS[groupName] = document.createElement('div');
@@ -31,6 +34,10 @@ function groupFonts(fontsListElement, rows) {
 
 		GROUP_ELEMENTS[groupName].appendChild(groupNameElement);
 		fontsListElement.appendChild(GROUP_ELEMENTS[groupName]);
+
+		const groupChangerOptionElement = document.createElement('span');
+		groupChangerOptionElement.innerHTML = GROUP_NAMES[groupName];
+		groupChangerElement.appendChild(groupChangerOptionElement);
 	}
 
 	for (const row of rows) {
@@ -40,10 +47,50 @@ function groupFonts(fontsListElement, rows) {
 
 		const fontName = nameElement.textContent;
 
-		// const textElementClasses = textElement.getAttribute('class').split(' ');
-
 		GROUP_ELEMENTS[fontName.charAt(1)].appendChild(row);
 
 		nameElement.textContent = fontName.substring(3);
+
+		row.addEventListener('click', changeGroup);
 	}
 }
+
+function changeGroup(event) {
+	if (
+		event.target !== clickedRowElement &&
+		event.target.parentElement !== clickedRowElement
+	) {
+		if (clickedRowElement != null)
+			clickedRowElement.classList.remove('fonts-list__row--active');
+
+		groupChangerElement.classList.add('open');
+
+		groupChangerElement.style.left = (event.pageX + 5).toString() + 'px';
+		groupChangerElement.style.top = (event.pageY + 5).toString() + 'px';
+
+		clickedRowElement = event.target;
+		if (event.target.className !== 'fonts-list__row')
+			clickedRowElement = event.target.parentElement;
+
+		clickedRowElement.classList.add('fonts-list__row--active');
+	} else {
+		groupChangerElement.classList.remove('open');
+		clickedRowElement.classList.remove('fonts-list__row--active');
+
+		clickedRowElement = null;
+	}
+}
+
+window.onclick = function(event) {
+	if (
+		!event.target.matches('.fonts-list__row') &&
+		!event.target.parentElement.matches('.fonts-list__row')
+	) {
+		groupChangerElement.classList.remove('open');
+
+		if (clickedRowElement != null)
+			clickedRowElement.classList.remove('fonts-list__row--active');
+
+		clickedRowElement = null;
+	}
+};
